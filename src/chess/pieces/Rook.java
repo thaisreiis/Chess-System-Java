da2a917fafc02ef44ba25ad612717cd81c984b67
Thais Reis <tdrv05@outlook.com>
2022-10-10 14:33:35 -0300

    Switching player each turn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -52,6 +52,16 @@ public class Rook extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
 		return mat;
 	}
 }
